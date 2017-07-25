d602ce7ec9e513e8e000fd7a3f8c55bb8b1e3cd8
April Ross <aprilross@aprils-mbp.lan>
2017-07-25 15:44:53 -0400

    initialcommit

---
+++ src/com/example/Main.java
@@ -0,0 +1,15 @@
+package com.example;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Month calendarMonth = new Month();
+
+        System.out.println("Please pick a starting month from the list below:");
+
+        System.out.println("Month Selections:" + englishName);
+
+
+    }
+}
